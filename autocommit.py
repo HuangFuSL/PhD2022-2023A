a6be633a83a8a47ce047c28ab58b96654fd908a7
HuangFuSL <huangfusl@outlook.com>
2022-10-14 14:36:49 +0800

    2022-10-14 14:36:49

--- autocommit.py
+++ autocommit.py
@@ -5,8 +5,16 @@ if __name__ == "__main__":
     # Get the current date and time
     now = datetime.datetime.now()
     string = now.strftime("%Y-%m-%d %H:%M:%S")
+    
+    current_branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).strip()
+    print(current_branch)
 
     # Commit the changes
     subprocess.call(['git', 'add', '.'])
     subprocess.call(['git', 'commit', '-m', string])
+    subprocess.call(['git', 'push', 'origin', current_branch])
+
+    # Pull and merge
+    subprocess.call(['git', 'pull', 'origin', 'main'])
+    subprocess.call(['git', 'merge', 'origin', 'main'])
     subprocess.call(['git', 'push', 'origin', 'main'])
